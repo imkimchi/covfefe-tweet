d3307d76cddb962ffc83909a22ddf5fa159997cf
imkimchi <wbvcos@gmail.com>
2017-06-01 18:28:22 -0400

    delete spinner

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
-import inquirer from 'inquirer'
 import covfefe from 'covfefe'
 import figlet from 'figlet'
 import chalk from 'chalk'
-import opn from 'opn'
+import fs from 'fs-extra'
 
+import tokenExists from './lib/tokenExist'
+import makeNewToken from './lib/newtoken'
 import generateTwit from './lib/twit'
-import question from './lib/question'
-import Spinner from './lib/spinner'
-import client from './lib/oauth'
 
-let authURL = "https://api.twitter.com/oauth/authenticate?oauth_token=";
 const log = console.log;
+const isNewLogin = () => fs.pathExists('./data.json')
 
-(async function donaldTrump () {
-    console.log(chalk.cyan(figlet.textSync('covfefe', {horizontalLayout: 'full'})))
-    //Spinner()
-
-    let oauth_token = await client.getOAuthRequestToken()
-    setTimeout(() => { opn(authURL+oauth_token[0]) }, 1300)
-    let answer = await inquirer.prompt(question)
-    let access_token
-
-    try {
-        access_token = await client.getOAuthAccessToken(oauth_token[0], oauth_token[1], answer.pin)
-    } catch (e) {
-        console.log("Sorry, PIN number is wrong")
-        process.exit(0)
-    }
+async function donaldTrump () {
+    log(chalk.cyan(figlet.textSync('covfefe', {horizontalLayout: 'full'})))
+    let answer = await isNewLogin() ? await tokenExists() : await makeNewToken()
+    let access_token = await fs.readJson('./data.json')
 
     try {
         let T = await generateTwit(access_token)
@@ -41,4 +28,6 @@ const log = console.log;
         console.error("Failed to update your status: ", e)
         process.exit(0)
     }
-})()
+}
+
+donaldTrump()
